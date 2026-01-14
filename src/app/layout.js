e0c13e32e83855ff7321ec2349bc59f9448c9570
yash1216 <yashpandit8631@gmail.com>
2026-01-14 17:19:32 +0530

    Add Google Analytics to RootLayout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,32 @@
 "use client";
 
+import Script from "next/script";
 import WhatsAppButton from "./components/whatsappButton";
 import "./globals.css";
-
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-94S5YHVRDH"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-94S5YHVRDH');
+          `}
+        </Script>
+      </head>
+
       <body>
         <main>{children}</main>
-        <WhatsAppButton/>
+        <WhatsAppButton />
       </body>
     </html>
   );

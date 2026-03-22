40aaf816e97e804c3139650a814c8eea7aeae4bc
MansiPathak <pathakmansi608@gmail.com>
2026-03-22 11:38:53 +0530

    updated

--- src/app/components/FranchiseApplication.js
+++ src/app/components/FranchiseApplication.js
@@ -269,19 +269,25 @@ const FranchiseApplication = () => {
   };
 
 const states = [
-  // States
+  "Andaman and Nicobar Islands",
   "Andhra Pradesh",
   "Arunachal Pradesh",
   "Assam",
   "Bihar",
+  "Chandigarh",
   "Chhattisgarh",
+  "Dadra and Nagar Haveli and Daman and Diu",
+  "Delhi",
   "Goa",
   "Gujarat",
   "Haryana",
   "Himachal Pradesh",
+  "Jammu and Kashmir",
   "Jharkhand",
   "Karnataka",
   "Kerala",
+  "Ladakh",
+  "Lakshadweep",
   "Madhya Pradesh",
   "Maharashtra",
   "Manipur",
@@ -289,6 +295,7 @@ const states = [
   "Mizoram",
   "Nagaland",
   "Odisha",
+  "Puducherry",
   "Punjab",
   "Rajasthan",
   "Sikkim",
@@ -297,19 +304,8 @@ const states = [
   "Tripura",
   "Uttar Pradesh",
   "Uttarakhand",
-  "West Bengal",
-
-  // Union Territories
-  "Andaman and Nicobar Islands",
-  "Chandigarh",
-  "Dadra and Nagar Haveli and Daman and Diu",
-  "Delhi",
-  "Jammu and Kashmir",
-  "Ladakh",
-  "Lakshadweep",
-  "Puducherry"
+  "West Bengal"
 ];
-
 
   // Check if form is valid by running validation
   const isFormValid = () => {
